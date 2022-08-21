a4d3a306f3b631d79659b34ec053bafca8ab5b4d
hjs3545 <hjs3545@gmail.com>
2022-08-21 23:59:55 +0900

    탭터치로 삭제하는 것 추가

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
 import { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Pressable } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]); /* 빈 배열을 넣은 usestate 이다. */
+  const [courseGoals, setCourseGoals] = useState(
+    []
+  ); /* 빈 배열을 넣은 usestate 이다. */
 
   function addGoalHandler(enteredGoalText) {
-    setCourseGoals((fff) => [                      /*setState 의 첫번째 파라미터는 현재의 state 구나*/
-      ...fff,
+    setCourseGoals((fff) => [
+      /*setState 의 첫번째 파라미터는 현재의 state 구나*/ ...fff,
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
   }
 
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} /> 
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
-          data={courseGoals}  /* 여태 courseGoals 에 넣은 배열을 리턴한다. */
+          data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+                id={itemData.item.id}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
           }}
-          alwaysBounceVertical={true}
+          alwaysBounceVertical={false}
         />
       </View>
     </View>
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -9,8 +9,10 @@ function GoalInput(props) {
   }
 
   function addGoalHandler() {
-    props.onAddGoal(enteredGoalText);   /* flatlist 를 렌더링하는 배열들을 조정하는 함수이다. */
-    setEnteredGoalText("");      /* 제출했으니 TextInput 폼을 비운다. */
+    props.onAddGoal(
+      enteredGoalText
+    ); /* flatlist 를 렌더링하는 배열들을 조정하는 함수이다. */
+    setEnteredGoalText(""); /* 제출했으니 TextInput 폼을 비운다. */
   }
 
   return (
@@ -18,8 +20,12 @@ function GoalInput(props) {
       <TextInput
         style={styles.textInput}
         placeholder="Your course goal!"
-        onChangeText={goalInputHandler}  /* 엔터고텍스트 변수를 타자가 변할때 마다 바꾸어서 문자열로 넣어준다 */
-        value={enteredGoalText}   /* 그래야 사용자가 본인이 타자를 치고 있음을 확인할 수 있다. */
+        onChangeText={
+          goalInputHandler
+        } /* 엔터고텍스트 변수를 타자가 변할때 마다 바꾸어서 문자열로 넣어준다 */
+        value={
+          enteredGoalText
+        } /* 그래야 사용자가 본인이 타자를 치고 있음을 확인할 수 있다. */
       />
       <Button title="Add Goal" onPress={addGoalHandler} />
     </View>
@@ -31,7 +37,7 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row", /* 옆에 나란히 있게 해주는 코드 */
+    flexDirection: "row" /* 옆에 나란히 있게 해주는 코드 */,
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 24,
